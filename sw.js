7b9bd7495e26b8524c9a9d17224268f94abe0f72
MarceloRoig <marceloroig80@gmail.com>
2019-11-22 17:39:03 -0300

    modificacion de iphone X+

--- sw.js
+++ sw.js
@@ -1,8 +1,8 @@
 
 importScripts('js/sw-utils.js')
 
-const STATIC_CACHE      = 'static-v3';
-const DYNAMIC_CACHE     = 'dynamic-v1';
+const STATIC_CACHE      = 'static-v4';
+const DYNAMIC_CACHE     = 'dynamic-v2';
 const INMUTABLE_CACHE   = 'inmutable-v1';
 
 //Aqui guardaremos los elementos estaticos, pero solo los que nosostros los desrrollamos, osea que podamos modificarlos con el tiempo.
@@ -45,6 +45,9 @@ self.addEventListener('activate', e => {
     const limpiaCaches = caches.keys().then( keys => {
         keys.forEach( key => {
             if( key !== STATIC_CACHE && key.includes('static') ) {
+                return caches.delete(key);
+            }
+            if( key !== DYNAMIC_CACHE && key.includes('dynamic') ) {
                 return caches.delete(key);
             }
         });
